03f52ffbd03304a86c9ddb6a8e270405b3d02f6b
Luiz Victor Aldenucci <105724710+DevtecAbrasel@users.noreply.github.com>
2023-11-24 12:00:45 -0300

    att

--- Credencia Abrasel/list-eventos.js
+++ Credencia Abrasel/list-eventos.js
@@ -285,3 +285,45 @@ document
       orb.fill = colorPalette.randomColor();
     });
   });
+
+
+  /*Botao de download----------------------------------------------------------------------------------- */
+
+  document.addEventListener("DOMContentLoaded",function(){
+    this.addEventListener("click",e => {
+      let tar = e.target;
+      if (tar.hasAttribute("data-dl")) {
+        let dlClass = "dl-working";
+        if (!tar.classList.contains(dlClass)) {
+          let lastSpan = tar.querySelector("span:last-child"),
+            lastSpanText = lastSpan.textContent,
+            timeout = getMSFromProperty("--dur",":root");
+  
+          tar.classList.add(dlClass);
+          lastSpan.textContent = "Baixando…";
+          tar.disabled = true;
+  
+          setTimeout(() => {
+            lastSpan.textContent = "Prontinho!";
+          },timeout * 0.9);
+  
+          setTimeout(() => {
+            tar.classList.remove(dlClass);
+            lastSpan.textContent = lastSpanText;
+            tar.disabled = false;
+          },timeout + 1e3);
+        }
+      }
+    });
+  });
+  function getMSFromProperty(property,selector) {
+    let cs = window.getComputedStyle(document.querySelector(selector)),
+      transDur = cs.getPropertyValue(property),
+      msLabelPos = transDur.indexOf("ms"),
+      sLabelPos = transDur.indexOf("s");
+  
+    if (msLabelPos > -1)
+      return transDur.substr(0,msLabelPos);
+    else if (sLabelPos > -1)
+      return transDur.substr(0,sLabelPos) * 1e3;
+  }
